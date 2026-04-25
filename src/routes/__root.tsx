749ddf17066fa37bf8197c70d1c157802c992877
Services-HLS <your-github-services@hithlakshsolutions.com>
2026-04-25 15:55:27 +0530

    Initial commit: Services-HLS project

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -44,7 +44,6 @@ export const Route = createRootRoute({
     ],
     links: [
       { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" },
-      { rel: "icon", href: "/favicon.ico" },
       { rel: "stylesheet", href: appCss },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
